83f3459dd5aa7c1d8ccd52205f7cfb97493a787f
ydp <ydp@7ecffa9e-50a7-d245-aed1-ee62579dfcb8>
2012-09-11 05:39:01 +0000

    git-svn-id: file:///c/Repositories/tableTestingAsset@10 7ecffa9e-50a7-d245-aed1-ee62579dfcb8

---
+++ TableTestingAsset/WebContent/js/nls/nls/testtable.js
@@ -0,0 +1,8 @@
+({
+"AccountBlance":"AccountBlance",
+"Rate":"Rate",
+"AccountOpeningDate":"AccountOpeningDate",
+"TradeTime":"TradeTime",
+"Usable":"Usable",
+"AccountName":"AccountName",
+"MaximumAmount":"MaximumAmount"})
---
+++ TableTestingAsset/WebContent/js/nls/nls/zh-cn/testtable.js
@@ -0,0 +1,8 @@
+({
+"AccountBlance":"\u8d26\u6237\u4f59\u989d",
+"Rate":"\u5229\u7387",
+"AccountOpeningDate":"\u5f00\u6237\u65e5\u671f",
+"TradeTime":"\u4ea4\u6613\u6b21\u6570",
+"Usable":"\u662f\u5426\u53ef\u7528",
+"AccountName":"\u8d26\u6237\u540d\u79f0",
+"MaximumAmount":"\u6700\u5927\u6570\u91cf"})
